ca471021d5b1613078ce1ec1b38f0b223604d4e7
ammarzafar4372 <ammarzafar4372@gmail.com>
2024-05-25 22:11:15 +0500

    Cell Added

--- graphics.py
+++ graphics.py
@@ -4,19 +4,40 @@ class Window:
     def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
         self.__root.title = "Maze Solver"
-        self.canvas = Canvas(bg="Black", height=height, width=width)
+        self.canvas = Canvas(bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW",self.close)
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self.running = True
         while self.running:
             self.redraw()
-        
-    def close(self):
+        print("Closing...")
+    
+    def draw_line(self, line: object, fill_color= "black")-> None:
+        line.draw(self.canvas, fill_color)
+
+    def close(self) -> None:
         self.running = False
+
+class Point:
+    def __init__(self, x:int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self,p1: object, p2: object) -> None:
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas: object, fill_color: str) -> None:
+        x1 = self.p1.x
+        y1 = self.p1.y 
+        x2 = self.p2.x 
+        y2 = self.p2.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800,800)
+    c = Cell(win)
+    c.draw(300, 300, 500, 500)
+    c1= Cell(win)
+    c1.draw(500,300,700,550)
+    c.draw_move(c1,True)
     win.wait_for_close()
 
 main()
